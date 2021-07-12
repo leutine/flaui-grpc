4abad882b97728dfff3d39369b883bc258d30136
Evgeniy Leutin <leutin.e@gmail.com>
2021-07-13 00:02:33 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+    
+}
